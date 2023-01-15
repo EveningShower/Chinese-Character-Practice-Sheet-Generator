93084fb2d0d6854acaca7029c880a572f4d91145
Youssef Akoudad <akoudadyf@gmail.com>
2023-01-15 19:34:56 +0100

    Update generate_chinese_character_practice.py

--- generate_chinese_character_practice.py
+++ generate_chinese_character_practice.py
@@ -1,37 +1,50 @@
 from PIL import Image, ImageDraw
 
-def generate_chinese_character_practice(paper_size, margins, square_size, filename, file_format):
+def generate_chinese_character_practice(
+    paper_size: tuple, margins: tuple, square_size: int, filename: str, file_format: str
+):
+    """
+    Generates a Chinese character practice sheet with custom paper size, margins, and square size.
+    :param paper_size: Tuple of width and height in pixels (default is A4 paper size)
+    :param margins: Tuple of width and height margins in pixels
+    :param square_size: Size of each square in pixels
+    :param filename: Name of the output file
+    :param file_format: Format of the output file (default is "png")
+    :return: None
+    """
     # Set the paper size and margins
     paper_width, paper_height = paper_size
     margins_width, margins_height = margins
 
     # Create a new image with a white background
-    img = Image.new('RGB', (paper_width, paper_height), 'white')
+    img = Image.new("RGB", (paper_width, paper_height), "white")
 
     # Create a single square image with gray lines
     gray = (192, 192, 192)
-    square = Image.new('RGB', (square_size, square_size), 'white')
+    square = Image.new("RGB", (square_size, square_size), "white")
     square_draw = ImageDraw.Draw(square)
-    square_draw.line((square_size/2, 0, square_size/2, square_size), fill=gray, width=2)
-    square_draw.line((0, square_size/2, square_size, square_size/2), fill=gray, width=2)
+    square_draw.line((square_size / 2, 0, square_size / 2, square_size), fill=gray, width=2)
+    square_draw.line((0, square_size / 2, square_size, square_size / 2), fill=gray, width=2)
     square_draw.line((0, 0, square_size, square_size), fill=gray, width=2)
     square_draw.line((0, square_size, square_size, 0), fill=gray, width=2)
     square_draw.rectangle((0, 0, square_size, square_size), outline=gray, width=2)
-    
+
     # Calculate the number of squares that will fit on the paper
     num_squares_width = (paper_width - (2 * margins_width)) // square_size
     num_squares_height = (paper_height - (2 * margins_height)) // square_size
     
     # Calculate the new margins so the image is symmetric
-    margins_width = int((paper_width - num_squares_width*square_size)/2)
-    margins_height = int((paper_height - num_squares_height*square_size)/2)
-    
+    margins_width = int((paper_width - num_squares_width * square_size) / 2)
+    margins_height = int((paper_height - num_squares_height * square_size) / 2)
+
     # Replicate the square image multiple times on the paper
-    for i in range(margins_width, margins_width+num_squares_width*square_size, square_size):
-        for j in range(margins_height, margins_height+num_squares_height*square_size, square_size):
+    for i in range(
+        margins_width, margins_width + num_squares_width * square_size, square_size
+    ):
+        for j in range(
+            margins_height, margins_height + num_squares_height * square_size, square_size
+        ):
             img.paste(square, (i, j))
 
     # Save the image
-    img.save(f'{filename}.{file_format}', dpi=(300,300))
-
-
+    img.save(f"{filename}.{file_format}", dpi=(300, 300))
